edf5e1cd201040002a66c5a4300a8de11eb793a3
Mike Kushner <mike760807@gmail.com>
2023-10-03 19:29:17 +0300

    добавлены запросы на создание таблиц

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int primary key,
+	first_name varchar(20),
+	last_name varchar(20),
+	title varchar(50),
+	birth_date date,
+	notes text
+);
+
+
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name text,
+	contact_name varchar(50)
+);
+
+
+CREATE TABLE orders
+(
+	order_id int primary key,
+	customer_id varchar(5) NOT NULL REFERENCES customers(customer_id),
+	employee_id int NOT NULL REFERENCES employees(employee_id),
+	order_date date,
+	ship_city varchar(30)
+);
